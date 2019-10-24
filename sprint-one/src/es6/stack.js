ec448121e86436d645102f5d7f1bc400937a13c6
Allen Tsai <hello.allen.tsai@gmail.com>
2019-10-23 21:29:01 -0700

    implemented stack es6

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,26 +2,25 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
-    this.counter = 0;
+    this.count = 0;
+    this.key = 0;
   }
 
-
-
-  push(value){
-    this[this.counter] = value;
-    this.counter++;
+  push(value) {
+    this[this.key] = value;
+    this.key++;
+    this.count++;
   }
 
-  pop (){
-    while(this.counter > 0){
-      var temp = this[this.counter-1];
-      this.counter--;
-      return temp;
+  pop() {
+    if (this.count > 0) {
+      let pop = this[--this.key];
+      this.count--;
+      return pop;
     }
   }
 
-  size(){
-    return this.counter;
+  size() {
+    return this.count < 0 ? 0 : this.count;
   }
-
 }
