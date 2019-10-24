d1cdeb98a976df9b086498f1aed62818a22e9fe7
Allen Tsai <hello.allen.tsai@gmail.com>
2019-10-23 23:11:26 -0700

    implemented tree; need to finish contain for nested children

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,17 +3,47 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];
+  extend(newTree, treeMethods);
 
   return newTree;
+};
+
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
+  return to;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  let child = Node(value);
+  this.children.push(child);
 };
 
 treeMethods.contains = function(target) {
+  // console.log(this.children[0].value); yields 5
+  var search = function(node) {
+    if (node.children.length > 0) {
+      for (let i = 0; i < node.children.length; i++) {
+        if (node.children[i].value === target) {
+          return true;
+        }
+      }
+    }
+    return false;
+  };
+  return search(this);
+};
+
+var Node = function(value) {
+  var node = {};
+  node.value = value;
+  node.children = [];
+  extend(node, treeMethods);
+  return node;
 };
 
 
