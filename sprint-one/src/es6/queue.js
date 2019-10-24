9009b9e6ab90c096edc35c45c55990ef0b6eb696
Allen Tsai <hello.allen.tsai@gmail.com>
2019-10-23 17:06:12 -0700

    queue and stack es6 implemented

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,28 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.count = 0;
+    this.next = 0;
+    this.key = 0;
   }
 
+  enqueue(value) {
+    this[this.key] = value;
+    this.key++;
+    this.count++;
+  }
+
+  dequeue() {
+    let dequeue = this[this.next];
+    this.next++;
+    this.count--;
+    return dequeue;
+  }
+
+  size() {
+    if (this.count < 0) {
+      this.count = 0;
+    }
+    return this.count;
+  }
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,26 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.counter = 0;
+  }
+
+
+
+  push(value){
+    this[this.counter] = value;
+    this.counter++;
+  }
+
+  pop (){
+    while(this.counter > 0){
+      var temp = this[this.counter-1];
+      this.counter--;
+      return temp;
+    }
+  }
+
+  size(){
+    return this.counter;
   }
 
 }
