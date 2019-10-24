613eee67a59a625b73a7aca1f881820088c4809d
Allen Tsai <hello.allen.tsai@gmail.com>
2019-10-24 16:37:53 -0700

    implemented set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,31 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = []; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  //if not contains, we add
+  if(!this.contains(item)){
+    this._storage.push(item);
+  }
+
 };
 
 setPrototype.contains = function(item) {
+  if (this._storage.indexOf(item) === -1) {
+    return false;
+  }
+  return true;
 };
 
 setPrototype.remove = function(item) {
+  let itemIdx = this._storage.indexOf(item);
+  if(itemIdx !== -1) {
+    this._storage.splice(itemIdx, 1);
+  }
 };
 
 /*
