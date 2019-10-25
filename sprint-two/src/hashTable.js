99f9cbbb2446f11aec7f717572ee6be11afc93ab
Allen Tsai <hello.allen.tsai@gmail.com>
2019-10-24 17:37:03 -0700

    implement hashtable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -9,7 +9,7 @@ var HashTable = function() {
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   if (this._storage[index] === undefined) {
-    this._storage[index] = [[k,v]];
+    this._storage[index] = [[k, v]];
   } else {
     this._storage[index].push([k, v]);
   }
@@ -20,7 +20,7 @@ HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   let arr = this._storage[index]; //[['bob', 'barker'], ['jim', 'elliot']]
   var result;
-  for (var i = 0; i < arr.length;i++) {
+  for (var i = 0; i < arr.length; i++) {
     if (arr[i][0] === k) {
       result = arr[i][1];
     }
