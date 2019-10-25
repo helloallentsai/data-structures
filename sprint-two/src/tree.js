a92dc7eaea60fb01a9c4fc1aa29621aacde4dbcf
Allen Tsai <hello.allen.tsai@gmail.com>
2019-10-25 16:22:31 -0700

    implemented parent property

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,3 +1,57 @@
+var Tree = function(value) {
+  var newTree = {};
+  newTree.value = value;
+  newTree.children = [];
+  newTree.parent = null;
+  extend(newTree, treeMethods);
+  return newTree;
+};
+
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
+  return to;
+};
+
+var treeMethods = {};
+
+treeMethods.addChild = function(value) {
+  let child = Tree(value);
+  child.parent = this.children;
+  this.children.push(child);
+};
+
+treeMethods.contains = function(target) {
+  let found = false;
+  let search = function(node) {
+    if (node.value === target) {
+      found = true;
+    }
+    for (let i = 0; i < node.children.length; i++) {
+      search(node.children[i]);
+    }
+  };
+  search(this);
+  return found;
+};
+
+// treeMethods.removeFromParent = function(tree) {
+//   let parent = tree.parent;
+//   let childIdx = parent.children.indexOf(tree.value);
+//   parent.children.splice(childIdx, 1);
+//   tree.parent = null;
+// };
+
+
+/*
+ * Complexity: What is the time complexity of the above functions?
+ addChild - O(1)
+ contains - O(n)
+ */
+
+
+//Shuwei's implementation
 // var Tree = function(value) {
 //   var newTree = {};
 //   newTree.value = value;
@@ -30,61 +84,3 @@
 // };
 
 
-var Tree = function(value) {
-  var newTree = {};
-  newTree.value = value;
-  newTree.children = [];
-  extend(newTree, treeMethods);
-  return newTree;
-};
-
-var extend = function(to, from) {
-  for (var key in from) {
-    to[key] = from[key];
-  }
-  return to;
-};
-
-var treeMethods = {};
-
-treeMethods.addChild = function(value) {
-  let child = Tree(value);
-  this.children.push(child);
-};
-
-treeMethods.contains = function(target) {
-  let found = false;
-  let search = function(node) {
-    if (node.value === target) {
-      found = true;
-    }
-    for (let i = 0; i < node.children.length; i++) {
-      search(node.children[i]);
-    }
-  };
-  search(this);
-  return found;
-
-  // var node = this;
-  // var queue = [node];
-
-  // while (queue.length) {
-  //   node = queue.shift();
-  //   if (node.value === target) {
-  //     return true;
-  //   }
-  //   if (node.children.length > 0) {
-  //     for (var i = 0; i < node.children.length; i++) {
-  //       queue.push(node.children[i]);
-  //     }
-  //   }
-  // }
-  // return false;
-};
-
-
-/*
- * Complexity: What is the time complexity of the above functions?
- addChild - O(1)
- contains - O(n)
- */
